3aea4595088da55f6bdd26869cf5409a7b996c60
Moemen Gaballa <Moemen.gaballa@expertapps.com.sa>
2024-02-16 18:43:58 +0200

    add routes and grom for connection DB Mysql , ORM

---
+++ apis.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func Posts(g *gin.Context) {
+
+}
+
+func Show(g *gin.Context) {
+
+}
+
+func Store(g *gin.Context) {
+
+}
+
+func Update(g *gin.Context) {
+
+}
+
+func Delete(g *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
 func main() {
+
+	// Connection Database
+	dsn := "root:@tcp(127.0.0.1:3306)/go-db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Error Connecting database")
+		fmt.Println(db) // TODO remove
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/posts", Posts)
+	r.GET("/posts/:id", Show)
+	r.POST("/posts", Store)
+	r.PUT("/posts/:id", Update)
+	r.DELETE("/posts/:id", Delete)
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
